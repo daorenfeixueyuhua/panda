03098a08e95b0c19e21214a2a3ceede9beb83de5
pengda <daorenfeixueyuhua@gmail.com>
2022-12-26 15:56:32 +0800

    feat: 增加读写分离配置（MasterSlaveAutoRoutingPlugin）

--- panda-common/panda-mybatis-starter/src/main/java/com/daoren/mybatis/config/MybatisPlusConfig.java
+++ panda-common/panda-mybatis-starter/src/main/java/com/daoren/mybatis/config/MybatisPlusConfig.java
@@ -1,5 +1,6 @@
 package com.daoren.mybatis.config;
 
+import com.baomidou.dynamic.datasource.plugin.MasterSlaveAutoRoutingPlugin;
 import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DynamicDataSourceProperties;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.core.incrementer.IKeyGenerator;
@@ -8,6 +9,7 @@ import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.daoren.mybatis.generator.MybatisPlusGenerator;
 import com.daoren.mybatis.interceptor.ParamsDataScopeInterceptor;
+import com.daoren.mybatis.interceptor.SqlLogInterceptor;
 import com.daoren.mybatis.properties.MybatisPlusGeneratorProperties;
 import com.daoren.mybatis.properties.MybatisPlusPageProperties;
 import org.apache.ibatis.plugin.Interceptor;
@@ -15,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 
 /**
  * MybatisPLusConfig
@@ -45,10 +48,18 @@ public class MybatisPlusConfig {
         return new H2KeyGenerator();
     }
 
+    /**
+     * MybatisPlus插件
+     *
+     * @return com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor
+     * @author peng_da
+     * @since 2022/11/10 9:45
+     */
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        interceptor.addInnerInterceptor(new SqlLogInterceptor());
         return interceptor;
     }
 
@@ -60,8 +71,28 @@ public class MybatisPlusConfig {
         return generator;
     }
 
+    /**
+     * 读写分离插件
+     *
+     * @return org.apache.ibatis.plugin.Interceptor
+     * @author peng_da
+     * @since 2022/12/26 15:19
+     */
+    @Bean(name = "masterSlaveAutoRoutingPlugin")
+    public Interceptor masterSlaveAutoRoutingPlugin() {
+        return new MasterSlaveAutoRoutingPlugin();
+    }
+
+    /**
+     * 自定义数据权限拦截器
+     *
+     * @return org.apache.ibatis.plugin.Interceptor
+     * @author peng_da
+     * @since 2022/12/26 15:41
+     */
+    @DependsOn(value = "masterSlaveAutoRoutingPlugin")
     @Bean(name = "paramsDataScopeInterceptor")
-    public Interceptor paramsDataScopeInterceptor(){
+    public Interceptor paramsDataScopeInterceptor() {
         // 可能需要调整bean的顺序
         return new ParamsDataScopeInterceptor();
     }
